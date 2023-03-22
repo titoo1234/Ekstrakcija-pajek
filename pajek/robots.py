b8821372e3cad9e38da971da118f4dc6f2ac4299
RandlD19 <damijanrandl@gmail.com>
2023-03-22 11:58:25 +0100

    roboti

--- pajek/robots.py
+++ pajek/robots.py
@@ -2,3 +2,16 @@ class Robots:
     def zadosca_robots_datoteki(self, link):
         # TODO dodaj omejitve!!!
         return True
+    
+    def vrni_nedovoljene_strani(self, url):
+        """
+        Metoda na podlagi url-ja poišče url/robots.txt datoteko, če obstaja
+        in vrne vse nedovoljene strani.
+        """
+        nedovoljene_strani = set([])
+        # TODO - poišči url/robots.txt in dodaj strani ki so pod "disallowed"
+        return nedovoljene_strani
+
+    # TODO - ustvari metode za ostale omejitve glede robots.txt datotek
+    # - user-agent
+    # - 
