742da505b278c1966bc76818452f64d25e55b91d
RandlD19 <damijanrandl@gmail.com>
2023-03-31 18:04:23 +0200

    app

--- pajek/app.py
+++ pajek/app.py
@@ -18,55 +18,7 @@ SEMENSKE_STRANI = ["http://gov.si/",
 ST_PAJKOV = 1
 DOMENA = "gov.si"
 
-
 if __name__ == '__main__':
     pajek = VecNitniPajek(SEMENSKE_STRANI, ST_PAJKOV, DOMENA)
     pajek.zazeni_pajka()
     pajek.info()
-
-# pot = pathlib.Path().absolute()
-# WEB_DRIVER_LOCATION = str(pot) + "\..\chromedriver.exe"
-# frontier = Frontier()
-# vmesnik = Vmesnik()
-# baza = Baza()
-
-
-# ZA IZBRIS PODATKOV IZ BAZE IN DODAJANJE SEED-OV    
-# ======================================================================
-#baza.zbrisi_vse_iz_baze(frontier,vmesnik)
-# ======================================================================
-# lastnosti = Options()
-# lastnosti.add_argument("--headless")
-# # ZANKA
-# vmesnik = webdriver.Chrome(WEB_DRIVER_LOCATION, options=lastnosti)
-
-#while True:
-
-# id, link = frontier.vrni_naslednjega()
-#VMESNIK. najdi  povezavoi
-#preden odprem stran, sparsam domeno
-# id_domena, domena = baza.poglej_domeno(link)
-# if id_domena == 0: #take domene še nimamo
-#     html_robot, sitemap = vmesnik.robot(link, domena)
-#     baza.dodaj_domeno(domena, html_robot, sitemap)
-
-
-# html = vmesnik.odpri_stran(link) #VRNE HTML
-# linki = vmesnik.poisci_povezave()
-# slike = vmesnik.poisci_slike()
-# PODATKI O STRANI HEAD...
-
-#SIMETRIČNO ZA SLIKE
-
-#IN DATOTEKE
-
-#NAFILI VSE V BAZO
-
-#PAZI VRSTNI RED
-# LAHKO MAMO SAMO ENO METODO V BAZA (KI SPREJME VELIKO ARGUMENTOV) IN POTEM V PRAVEM VRSTEM REDU KLICE SVOJE METODE (FILA BAZOs)
-#baza.dodaj_vse_v_bazo(link, html, slike) #id nerabis ker tisto je id od frontierja
-# frontier.dodaj_vec_linkov(linki, id_domena)
-# frontier.obdelan_link(id)
-
-
-
