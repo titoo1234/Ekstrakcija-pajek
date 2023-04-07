840e19a239294eb55dc45e1caec26bf449bad355
marinkovicm <marko.marinkovic50@gmail.com>
2023-04-07 12:21:48 +0200

    spremenjeno da delajo commandline argsi i guess

--- pajek/app.py
+++ pajek/app.py
@@ -1,4 +1,5 @@
 from vec_nitni_pajek import VecNitniPajek
+import sys
 
 SEMENSKE_STRANI = ["http://gov.si/",
                    "http://evem.gov.si/",
@@ -9,6 +10,15 @@ DOMENA = "gov.si"
 
 if __name__ == '__main__':
     semenske_strani = False
+    try:
+        ST_PAJKOV = int(sys.argv[1])
+        semenske_strani = sys.argv[2]
+        if semenske_strani == "T":
+            semenske_strani = True
+        else:
+            semenske_strani = False
+    except:
+        pass
     pajek = VecNitniPajek(SEMENSKE_STRANI, ST_PAJKOV, DOMENA, semenske_strani)
     if not semenske_strani:
         pajek.zazeni_pajka()
