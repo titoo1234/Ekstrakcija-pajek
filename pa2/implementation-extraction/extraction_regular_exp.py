8081584bc2569d549ed6fc837e5a44df01479217
titoo1234 <tito.arnsek@gmail.com>
2023-04-14 22:47:40 +0200

    ekstrakcija z regularnimi izrazi

--- pa2/implementation-extraction/extraction_regular_exp.py
+++ pa2/implementation-extraction/extraction_regular_exp.py
@@ -1,15 +1,112 @@
 # FUNKCIJE ZA REGULARNE
 # 3 funkcije za vsak par strani
-
-
+import re
+import codecs
+import regex
+import json
 def rtv_slo(html):
-    pass
+    slovar = dict()
 
+    title_pattern = r"<h1>(.*)<\/h1>\s*<div class=\"subtitle\">"
+    match = re.compile(title_pattern).search(html)
+    title = match.group(1)
+    slovar['title'] = title
+    # print(title)
 
+    subtitle_pattern = r"<div class=\"subtitle\">(.*)<\/div>"
+    match = re.compile(subtitle_pattern).search(html)
+    subtitle = match.group(1)
+    slovar['subtitle'] = subtitle
+    # print(subtitle)
+
+    author_pattern = r"<div class=\"author-timestamp\">\s+<strong>(.*)<\/strong>"
+    match = re.compile(author_pattern).search(html)
+    author = match.group(1)
+    slovar['author'] = author
+    # print(author)
+
+    publishedTime_pattern = r"<\/strong>\|\s+(.*)\s*<\/div>\s*<div class=\"place-source\">"
+    match = re.compile(publishedTime_pattern).search(html)
+    publishedTime = match.group(1)
+    slovar['publishedTime'] = publishedTime
+    # print(publishedTime)
+
+    lead_pattern = r"<p class=\"lead\">(.+)<\/p>"
+    match = re.compile(lead_pattern).search(html)
+    lead = match.group(1)
+    slovar['lead'] = lead
+    # print(lead)
+
+    content_pattern = r"<p class=\"Body\">([\s\S]+?)<\/p>"
+    # content_pattern = r"(?:(?<=^)|(?<=\W))<article(.*?)(<p.*?>(.+?)<\/p.*?>)+?(.*?)(?=<\/article>)(?:(?=\W)|(?=$))"
+    contents = re.finditer(content_pattern, html)
+    all_content = ''    
+    for content in contents:
+        all_content += content.group(1)
+    slovar['content'] = all_content
+
+    return slovar
 def overstock(html):
+    '''
+    '''
+    title = r"</tbody></table></td><td valign=\"top\">\s*<a.*>\s*<b>(.*)<\/b>"
+    # "<td valign=\"top\">\s*<a.*>\s*<b>(.*)<\/b>
+    listPrice = r"[\s\S|.]*?<td align=\"left\" nowrap=\"nowrap\">\s*<s>(.*)<\/s>"
+    # [\s\S|.]*?<td align=\"left\" nowrap=\"nowrap\">\s*<s>(.*)<\/s>
+    price = r"[\s\S|.]*?<span class=\"bigred\">\s*<b>(.*)<\/b>"
+    # \s*<\/td>[\s\S|.]*?<span class=\"bigred\">\s*<b>(.*)<\/b>
+    saving = r"[\s\S|.]*?<span class=\"littleorange\">([$€]\s*[0-9\.,]+).*"
+    # [\s\S|.]*?<span class=\"littleorange\">([$€]\s*[0-9\.,]+)
+    savingPercent = r"\((.*?)\)"
+    # \((.*)\)<\/span>[\s\S|.]*?
+    content = r"[\s\S|.]*?<span class=\"normal\">([\s\S|.]*?)<br>"
+    #  <span class=\"normal\">([\s\S|.]*?)<br>"
+    regex = title+listPrice+price+saving+savingPercent+content
+    slovar = dict()
+    counter = 1
+    # regex = r"<td valign=\"top\">\s*<a.*>\s*<b>(.*)<\/b>[\s\S|.]*?<td align=\"left\" nowrap=\"nowrap\">\s*<s>(.*)<\/s>\s*<\/td>[\s\S|.]*?<span class=\"bigred\">\s*<b>(.*)<\/b>[\s\S|.]*?<span class=\"littleorange\">([$€]\s*[0-9\.,]+) \((.*)\)<\/span>[\s\S|.]*?<span class=\"normal\">([\s\S|.]*?)<br>"
+    matches = re.finditer(regex, html)
+    item = dict()
+    for match in matches:
+        title = match.group(1)
+        item['title'] = title
 
-    # dictionary
+        listPrice = match.group(2)
+        item['listPrice'] = listPrice
+        
+        price = match.group(3) 
+        item['price'] = price
 
-    # return dict/dict.tojson()
-    pass
+        saving = match.group(4)
+        item['saving'] = saving
+        
+        savingPercent = match.group(5)
+        item['savingPercent'] = savingPercent
+        
+        content = match.group(6)
+        item['content'] = content
+
+        slovar['item ' + str(counter) ] = item
+        counter += 1
+        item = dict()
+        
+    
+    return slovar
+
+
+if __name__ == '__main__':
+    path1 = '../input-extraction/WebPages/overstock.com/jewelry01.html'
+    path1 = r'C:\Users\Uporabnik\Desktop\IŠRM 1\Ekstrakcija\Ekstrakcija-pajek\pa2\input-extraction\WebPages\overstock.com\jewelry01.html'
+    path2 = r'C:\Users\Uporabnik\Desktop\IŠRM 1\Ekstrakcija\Ekstrakcija-pajek\pa2\input-extraction\WebPages\overstock.com\jewelry02.html'
+    htmls = [path1,path2]
+    path = r'C:\Users\Uporabnik\Desktop\IŠRM 1\Ekstrakcija\Ekstrakcija-pajek\pa2\input-extraction\WebPages\rtvslo.si\Audi A6 50 TDI quattro_ nemir v premijskem razredu - RTVSLO.si.html'
+    # path = r'C:\Users\Uporabnik\Desktop\IŠRM 1\Ekstrakcija\Ekstrakcija-pajek\pa2\input-extraction\WebPages\overstock.com\jewelry01.html'
+    # with open(path, 'r',encoding='utf-8') as file:
+    #     pageContent = file.read()
+    # rtv_slo(pageContent)
+
+    pageContent = codecs.open(path, 'r', encoding='utf-8', errors='ignore').read()
+    json_object = json.dumps(rtv_slo(pageContent), indent = 4) 
+    # rtv_slo(pageContent)
+    print(json_object)
 
