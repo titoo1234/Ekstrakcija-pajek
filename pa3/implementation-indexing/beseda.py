adf943d03ab6a351366ca5221142ef39c0cf0bcc
RandlD19 <damijanrandl@gmail.com>
2023-05-19 18:26:41 +0200

    beseda

--- pa3/implementation-indexing/beseda.py
+++ pa3/implementation-indexing/beseda.py
@@ -2,15 +2,19 @@ from baza import Baza
 from bs4 import BeautifulSoup
 from nltk.tokenize import word_tokenize
 class Beseda():
-    def __init__(self,beseda,dokument,frekvenca,indeks):
+    def __init__(self,beseda,dokument):
         self.beseda = beseda
         self.dokument = dokument
-        self.frekvenca = frekvenca
-        self.indeks = indeks
+        self.frekvenca = 0
+        self.indeks = ""
 
     def dodaj_v_bazo(self):
         if not Baza.preveri_besedo(self):
             Baza.dodaj_besedo_v_index_word(self)
         Baza.dodaj_besedo_v_bazo(self)
+
+    def nastavi_indeks(self):
+        
+    
 
     
