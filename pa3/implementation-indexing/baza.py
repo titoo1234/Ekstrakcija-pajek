ac4d1fceec5c34793112be1854e6c3db0f552d57
titoo1234 <tito.arnsek@gmail.com>
2023-05-20 08:55:19 +0200

    a

--- pa3/implementation-indexing/baza.py
+++ pa3/implementation-indexing/baza.py
@@ -1,5 +1,6 @@
 import sqlite3
 # from beseda import Beseda
+# TODO potrebno no shranjevati beseda.lower() to bo potrebno najprej tokens_celotni preuredit 
 class Baza():
     def __init__(self):
         self.conn = sqlite3.connect('inverted-index.db',isolation_level=None)
@@ -8,20 +9,20 @@ class Baza():
     def dodaj_besedo_v_bazo(self,beseda):
         poizvedba = '''INSERT into Posting (word, documentName,frequency,indexes) values (?,?,?,?)'''
         cursor = self.conn.cursor()
-        cursor.execute(poizvedba,(beseda.beseda.lower(),beseda.dokument.ime,beseda.frekvenca,beseda.indeks))
+        cursor.execute(poizvedba,(beseda.beseda,beseda.dokument.ime,beseda.frekvenca,beseda.indeks))
         cursor.close()
 
     def preveri_besedo(self,beseda):
         poizvedba = '''SELECT count(*) from IndexWord where word = ?'''
         cursor = self.conn.cursor()
-        cursor.execute(poizvedba,[beseda.beseda.lower()])
+        cursor.execute(poizvedba,[beseda.beseda])
         rez = cursor.fetchone()[0]
         return  rez == 1
     
     def dodaj_besedo_v_index_word(self,beseda):
         poizvedba = '''INSERT into IndexWord values (?)'''
         cursor = self.conn.cursor()
-        cursor.execute(poizvedba,(beseda.beseda.lower(),))
+        cursor.execute(poizvedba,(beseda.beseda,))
         cursor.close()
     
     def dodaj_dokument(self,dokument,text,tokens):
