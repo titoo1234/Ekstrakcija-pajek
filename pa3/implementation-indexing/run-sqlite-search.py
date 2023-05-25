86ef5efb37b19aed64c1a7b9c804d655ff0118e0
titoo1234 <tito.arnsek@gmail.com>
2023-05-25 14:44:04 +0200

    lepši izpis

--- pa3/implementation-indexing/run-sqlite-search.py
+++ pa3/implementation-indexing/run-sqlite-search.py
@@ -8,7 +8,6 @@ import os
 import os.path
 import re
 
-<<<<<<< HEAD
 def uredi_besedilo(besedilo):
     # Odstrani presledke med ločili
     uredeno_besedilo = re.sub(r'\s+([.,:;?!])', r'\1', besedilo)
@@ -16,16 +15,6 @@ def uredi_besedilo(besedilo):
     uredeno_besedilo = uredeno_besedilo.strip()
     return uredeno_besedilo
 
-=======
->>>>>>> 5a45857b61f0fe34ea09ff0fd8c12c654835ca89
-def naredi_slovar_poti():
-    ''' Funkcija vsakemu dokumentu pripiše njegovo ustrezno pot'''
-    slovar = {}
-    for mapa in os.listdir(POT):
-        pot = os.path.join(POT, mapa)
-        for dokument in os.listdir(pot): # treba je se počistit __MACOS in druge
-            slovar[dokument] = pot
-    return slovar
 
 def razbij_poizvedbo_na_besede(niz):
     ''' Metoda razdeli niz na posamezne besede in vrne tabelo besed '''
@@ -76,15 +65,17 @@ def uredi_indekse(tab):
     return pari
         
 def izpisi_snipet(indeksi,tokens,tekst):
-    snipet  = ''
+    snipet  = []
     indeksi.sort()
     indeksi = uredi_indekse(indeksi) 
     # print(indeksi)
     for levi,desni in indeksi: 
         
         # snipet += '...' + tekst[index-30:index+30] + '...\n'
-        snipet +=  '...'+ uredi_besedilo(' '.join(tokens[levi-2:desni+3]))+ '...\n'
-    print(snipet)
+        snipet.append(uredi_besedilo(' '.join(tokens[levi-2:desni+3])))
+    return "... " + " ... ".join(snipet) + " ..."
+    #     snipet +=  '...'+ uredi_besedilo(' '.join(tokens[levi-2:desni+3]))+ '...\n'
+    # print(snipet)
 
 
 if __name__ == '__main__':
@@ -94,37 +85,46 @@ if __name__ == '__main__':
     tab = [1]
     # print(uredi_indekse(tab))
     
-    
-    while True:
-        vhod = input("Results for a query: ")
-        print("\n\n")
-        zacetek = time()
-        # pridobivanje podatkov iz baze...
-        tab_besed = razbij_poizvedbo_na_besede(vhod)
-        slovar_dokumenti = dict()
-        for beseda in tab_besed:
-            for vrstica in poisci_podatke(beseda):
-                beseda,dokument,frekvenca,indeksi,tekst,tokens_celoten = vrstica #rezultat = (beseda,dokument,frekvenca,indeksi,tekst,tokens)
-                if dokument in slovar_dokumenti: 
-                    slovar_dokumenti[dokument][2].extend([int(i) for i in indeksi.split(',')]) 
-                else:
-                    slovar_dokumenti[dokument] = (tekst,tokens_celoten,[int(i) for i in indeksi.split(',') if indeksi != ''])
-        # print(slovar_dokumenti)
-        sorted_dict = dict(sorted(slovar_dokumenti.items(), key=lambda item: len(item[1][2]),reverse=True))
-        i = 0
-        for dokument,(tekst,tokens,indeksi) in sorted_dict.items():
-            print(dokument)
-            print(len(indeksi))
-            tokens = tokens.split(',,,')
-            pravi_tokens = []
-            for tab in tokens:
-                pravi_tokens.extend(tab.split(','))
-                pravi_tokens.append(',')
-            pravi_tokens = pravi_tokens[:-1] 
-            izpisi_snipet(indeksi,pravi_tokens,tekst)
-            i+=1
-            if i > 3:
-                break
+
+# while True:
+    vhod = input("Results for a query: ")
+    print("\n\n")
+    zacetek = time()
+    # pridobivanje podatkov iz baze...
+    tab_besed = razbij_poizvedbo_na_besede(vhod)
+    slovar_dokumenti = dict()
+    for beseda in tab_besed:
+        for vrstica in poisci_podatke(beseda):
+            beseda,dokument,frekvenca,indeksi,tekst,tokens_celoten = vrstica #rezultat = (beseda,dokument,frekvenca,indeksi,tekst,tokens)
+            if dokument in slovar_dokumenti: 
+                slovar_dokumenti[dokument][2].extend([int(i) for i in indeksi.split(',')]) 
+            else:
+                slovar_dokumenti[dokument] = (tekst,tokens_celoten,[int(i) for i in indeksi.split(',') if indeksi != ''])
+    # print(slovar_dokumenti)
+    sorted_dict = dict(sorted(slovar_dokumenti.items(), key=lambda item: len(item[1][2]),reverse=True))
+    i = 0
+
+
+    glava = ["Frequencies", "Document", "Snippet"]
+    tabela = []
+    for dokument,(tekst,tokens,indeksi) in sorted_dict.items():
+        tokens = tokens.split(',,,')
+        pravi_tokens = []
+        for tab in tokens:
+            pravi_tokens.extend(tab.split(','))
+            pravi_tokens.append(',')
+        pravi_tokens = pravi_tokens[:-1] 
+        snipet = izpisi_snipet(indeksi,pravi_tokens,tekst)
+        tabela.append([len(indeksi),dokument, snipet])#frekvenca,dokument,snipet
+        # i+=1
+        # if i > 3:
+        #     break
+    konec = time() 
+    print(f"\nResults found in {round(konec-zacetek,0)}s\n\n")
+    with open("testne_dat_sql.txt", "w") as dat:
+        print(tabulate(tabela, headers=glava), file=dat)
+
+
         
 
          
